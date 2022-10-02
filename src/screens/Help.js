685ef2c9435c95da4b17ea2f46a8a136993db3df
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-10-02 07:27:10 +0000

    done with the help-accordion

--- src/screens/Help.js
+++ src/screens/Help.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   SafeAreaView,
   Dimensions,
+  ScrollView,
 } from "react-native";
 import React from "react";
 import {
@@ -51,8 +52,18 @@ const topics = [
 ];
 
 const Help = () => {
-  const [open, setOpen] = React.useState(false);
+  const keyPressRef = React.useRef(null);
+  const [selected, setSelected] = React.useState(null);
   const navigation = useNavigation();
+
+  const handleOnPress = (index) => {
+    if (selected === index) {
+      return setSelected(null);
+    } else {
+      return setSelected(index);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity
@@ -62,29 +73,41 @@ const Help = () => {
         <MaterialIcons name="keyboard-arrow-left" size={30} color="#000" />
       </TouchableOpacity>
       <Text style={styles.title}>Help</Text>
-      <View style={styles.topicsWrapper}>
-        <Text style={styles.allTopics}>All Topics</Text>
-        {topics?.map((topic, index) => (
-          <TouchableOpacity key={index} style={styles.topicsContainer}>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <AntDesign name="bars" size={24} color="black" />
-              <Text style={styles.topic}>{topic.title}</Text>
+      <ScrollView style={{ flex: 1, backgroundColor: "#e6e7e8" }}>
+        <View style={styles.topicsWrapper}>
+          <Text style={styles.allTopics}>All Topics</Text>
+          {topics?.map((topic, index) => (
+            <View key={index}>
+              <TouchableOpacity
+                style={styles.topicsContainer}
+                onPress={() => handleOnPress(index)}
+                ref={keyPressRef}
+              >
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <AntDesign name="bars" size={24} color="black" />
+                  <Text style={styles.topic}>{topic.title}</Text>
+                </View>
+                <View>
+                  {index === selected ? (
+                    <MaterialIcons
+                      name="keyboard-arrow-down"
+                      size={26}
+                      color="black"
+                    />
+                  ) : (
+                    <MaterialIcons
+                      name="keyboard-arrow-right"
+                      size={26}
+                      color="black"
+                    />
+                  )}
+                </View>
+              </TouchableOpacity>
+              <Text>{selected === index ? topic.dropdown : null}</Text>
             </View>
-            <View>
-              {!open ? (
-                <MaterialIcons
-                  name="arrow-forward-ios"
-                  size={24}
-                  color="black"
-                />
-              ) : (
-                <MaterialIcons name="arrow-back-ios" size={24} color="black" />
-              )}
-            </View>
-            <Text>{topic?.dropdown}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -98,6 +121,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     textAlign: "center",
+    fontWeight: "bold",
+    paddingBottom: 5,
   },
   arrowContainer: {
     height: 40,
@@ -119,11 +144,15 @@ const styles = StyleSheet.create({
   topic: {
     fontSize: 16,
     paddingLeft: 10,
+    fontWeight: "500",
   },
-  allTopics: { marginVertical: 10 },
+  allTopics: {
+    marginVertical: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
   topicsWrapper: {
     justifyContent: "center",
-    height: height / 2,
     marginHorizontal: 10,
     marginVertical: 20,
   },
