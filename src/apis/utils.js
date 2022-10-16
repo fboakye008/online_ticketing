04480b415eee9660c577ba0a53edf41c99dea0b8
Frank Boakye <fboakye008@st.ug.edu.gh>
2022-10-16 18:50:41 -0400

    End to end creating of ticket. Only works for a single passenger for now

--- src/apis/utils.js
+++ src/apis/utils.js
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {API_URL} from "@env";
+import _ from "underscore";
 
 
 const self = module.exports = {
@@ -13,17 +14,23 @@ const self = module.exports = {
         if (savedUser) {
             return JSON.parse(savedUser);
         }
-        throw new Error("THE system has logged you out of the system. Please log back in");
+        throw new Error("You have been logged out of the system. Please log back in!");
     },
-
+   uniquify : function (objArray) {
+        let result = objArray.map(a => ({"value":a.route_id, "label": a.route}));
+        return _.uniq(result, function (x) {
+            return x["value"];
+        });
+    },
     /**
      * MAke a request to the API
      * @returns {Promise<null|any>}
      */
     makeAPIRequest: async function (params) {
-
         try {
+            console.log("API_URL",`${API_URL}`);
             const url = `${API_URL}/${params.url}`;
+            console.log("url",url);
             const options = {};
             options.method = params.method;
 
@@ -47,119 +54,6 @@ const self = module.exports = {
             console.log(error);
             throw error
         }
-    },
-    /**
-     *
-     * @param user_key
-     * @param allowedRoles
-     * @returns {Promise<boolean>}
-     */
-    // hasPermission: async function (user_key, allowedRoles) {
-    //     const app = require('../../server/server');
-    //     const serverUtils = require('../../server/utils/utils');
-    //     try {
-    //         const user = await app.models.Users.findOne({where: {api_key: user_key}});
-    //         if (!user) {
-    //             throw new Error("Invalid API key.");
-    //         }
-    //         const isAllowed = serverUtils.isAllowed(user, allowedRoles);
-    //         if (isAllowed) {
-    //             return true;
-    //         }
-    //         throw new Error("You don't have access to this resource.");
-    //     } catch (ee) {
-    //         throw ee
-    //     }
-    // },
-    // findDuplicates: function (arry) {
-    //     const set = new Set(arry);
-    //     const duplicates = arry.filter(item => {
-    //         if (set.has(item)) {
-    //             set.delete(item);
-    //         } else {
-    //             return item;
-    //         }
-    //     });
-    //     return duplicates;
-    // },
-    // /**
-    //  *
-    //  * @param ctx
-    //  * @returns {Promise<boolean>}
-    //  */
-    // prism_authorization: async function (ctx) {
-    //     const req = ctx.req;
-    //     const api_key = req.headers['user_key'] || req.query.user_key || req.body.user_key;
-    //     if (api_key) {
-    //         return await self.hasPermission(api_key, ["MACHINE", "ADMIN", "CMAP_CORE", "PRISM"]);
-    //     } else {
-    //         return false;
-    //     }
-    // },
-    // mts_authorization: async function (ctx) {
-    //     const req = ctx.req;
-    //     const api_key = req.headers['user_key'] || req.query.user_key || req.body.user_key;
-    //     if (api_key) {
-    //         return await self.hasPermission(api_key, ["MACHINE", "ADMIN", "CMAP_CORE", "MTS_MANAGER"]);
-    //     } else {
-    //         return false;
-    //     }
-    // },
-    // prism_before_remote: async function (ctx) {
-    //     const allowed = await self.prism_authorization(ctx);
-    //     if (allowed) {
-    //         return "done";
-    //     }
-    //     throw new Error("Do not have permission to view resource");
-    // },
-    // /**
-    //  *
-    //  * @param dataSource
-    //  * @param sqlStatement
-    //  * @param params
-    //  * @param options
-    //  * @returns {Promise<unknown>}
-    //  */
-    // executeAsync: async function (dataSource, sqlStatement, params, options) {
-    //     return new Promise((resolve, reject) => {
-    //         dataSource.connector.execute(sqlStatement, params, options, (err, result) => {
-    //             if (err) reject(err);
-    //             else resolve(result);
-    //         });
-    //     });
-    // },
-    // uniquify: function (list2Uniquify,element) {
-    //     return _.uniq(list2Uniquify, function (x) {
-    //         return x[element];
-    //     });
-    // },
-    // /**
-    //  *
-    //  * @param map
-    //  * @returns {[]}
-    //  */
-    // createInsertStatementFromMap: function (map) {
-    //     const sqlValues = [];
-    //     for (const [fieldKey, values] of map.entries()) {
-    //         if (values && values.length > 0) {
-    //             const fieldValues = _.uniq(values);
-    //             for (let fieldValue of fieldValues) {
-    //                 sqlValues.push("(" + fieldKey + "," + fieldValue + ")");
-    //             }
-    //         }
-    //     }
-    //     return sqlValues;
-    // },
-    // createSelectStatement: function (collection, tableName,fieldName) {
-    //
-    //     const field = fieldName? fieldName : "name";
-    //     let sqlstatement = "";
-    //     if (collection && collection.length > 0) {
-    //         sqlstatement = "SELECT distinct id FROM " + tableName + " WHERE " + field + " in (";
-    //         const stmtArray = _.map(collection, function(num){ return "'" + num.trim() + "'"});
-    //         sqlstatement = sqlstatement + stmtArray.join(",") + ");"
-    //     }
-    //     return sqlstatement;
-    // }
+    }
 };
 module.exports = self;
