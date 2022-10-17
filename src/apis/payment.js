06faa696d6948dd64579990b811e6a1ca1b0f5d7
Frank Boakye <fboakye008@st.ug.edu.gh>
2022-10-16 20:26:55 -0400

    foo

--- src/apis/payment.js
+++ src/apis/payment.js
@@ -1,44 +1,42 @@
 import utils from "./utils";
 
-async function CreatePayment(paymentInfo,num_passengers) {
+async function CreatePayment(paymentInfo) {
     const fetchAPI = async (payment) => {
         const options = {
-            url : "bookings/" + payment.bookingId + "/payment",
-            method : "POST",
+            url: "bookings/" + payment.bookingId + "/payment",
+            method: "POST",
             body: payment
         }
         return await utils.makeAPIRequest(options)
     };
-    const promises = [];
-    for(let index=0; index < num_passengers; index++){
-        const payment = {
-            bookingId: paymentInfo.bookingId,
-            payment_method: paymentInfo.payment_method,
-            amount_paid: paymentInfo.amount_paid,
-        };
-        promises.push(fetchAPI(payment));
-    }
-    return await Promise.all(promises);
+    const payment = {
+        bookingId: paymentInfo.bookingId,
+        payment_method: paymentInfo.payment_method,
+        amount_paid: paymentInfo.amount_paid,
+    };
+    return await fetchAPI(payment);
 }
-async function CreateTickets(payments){
+
+async function CreateTickets(payments) {
     const promises = [];
 
 
     const fetchAPI = async (ticket) => {
         const options = {
-            url : "bookings/" + ticket.bookingId + "/tickets",
-            method : "POST",
+            url: "bookings/" + ticket.bookingId + "/tickets",
+            method: "POST",
             body: ticket
         }
         return await utils.makeAPIRequest(options)
     };
 
-    for(let payment of payments){
-        const ticket ={
+    for (let payment of payments) {
+        const ticket = {
             bookingId: payment.bookingId
         };
         promises.push(fetchAPI(ticket));
     }
     return await Promise.all(promises);
 }
-export {CreatePayment,CreateTickets};
+
+export {CreatePayment, CreateTickets};
